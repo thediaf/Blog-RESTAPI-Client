b4f37748d84029a6c1e0d8de0673d531e2e60590
thediaf <jaharacs98@gmail.com>
2022-07-25 22:15:19 +0000

    article show

--- View/show.php
+++ View/show.php
@@ -1,21 +1,19 @@
 <?php ob_start() ?> 
-    <div class="col-md-offset-1 col-md-10">
-        <article class="">
-            <h2>
-                <a href="../public/index.php?route=article&idArt=<?= htmlspecialchars($article->id);?>" class="chapter">
-                    <?= htmlspecialchars($article->titre);?>
-                </a>
-            </h2>
-            <p>
-                <?= $article->contenu?>
-            </p>
-            <p>
-                <strong>Date d'ajout :</strong> 
-                <?= htmlspecialchars($article->dateCreation);?>
-            </p>
-        </article>
-    </div>
 
+    <a href="../index.php?action=show&id=<?= htmlspecialchars($article->id);?>" 
+            class="block p-6 mt-6 w-full bg-white rounded-lg border border-gray-200 
+            shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    >
+        <h5 class="mb-2 text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">
+            <?= $article->titre;?>
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400">
+            <?= $article->contenu?>
+        </p><br>
+        <p class="font-normal text-right text-gray-700 dark:text-gray-400">
+            Date publication: <?= $article->dateCreation?>
+        </p>
+    </a>
 
 <?php $content = ob_get_clean(); ?>
 <?php require('View/template.php'); ?>
